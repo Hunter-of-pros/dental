499af65cc94109f2ac53812ae856b54980db9baf
Hunter-of-pros <vikasm8660@gmail.com>
2026-02-18 20:20:49 +0530

    Add routes for Treatment, Our Clinics, Patient Safety, Doctors, and Contact pages

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,12 +1,24 @@
 import React from 'react'
 import { Route,Routes } from 'react-router-dom'
 import Home from './pages/Home'  
+import Treatment from './pages/Treatment'
+import OurClinics from './pages/OurClinics'
+import PatientSafety from './pages/PatientSafety'
+import Doctors from './pages/Doctors'
+import Contact from './pages/Contact'
+
 
 const App = () => {
   return (
     <div className='mx-4 sm:mx-[10%]'>
       <Routes>
         <Route path='/' element={<Home/>}/>
+        <Route path='/treatment' element={<Treatment/>}/>
+        <Route path='/our-clinics' element = {<OurClinics/>}/>
+        <Route path='/patient-safety' element = {<PatientSafety/>}/>
+        <Route path='/doctors' element = {<Doctors/>}/>
+        <Route path='/contact' element = {<Contact/>}/>
+        
       </Routes>
     </div>
   )
